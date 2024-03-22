bcf359fe8f246f5197d4fe03bcee92127347e2cf
TiagoPaulin <thiago.vmp15@gmail.com>
2024-03-22 17:10:19 -0300

    primeiro commit

---
+++ src/test/java/com/apirestful/apirestful/ApirestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestful.apirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
