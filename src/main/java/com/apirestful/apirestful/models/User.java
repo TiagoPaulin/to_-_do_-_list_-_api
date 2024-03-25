7c789813fd132c6dfdda97845c3acddb6afa0670
TiagoPaulin <thiago.vmp15@gmail.com>
2024-03-25 11:41:07 -0300

    Criação do service o user

--- src/main/java/com/apirestful/apirestful/models/User.java
+++ src/main/java/com/apirestful/apirestful/models/User.java
@@ -44,7 +44,7 @@ public class User {
     public User(){}
 
     // metodos get
-    public long getId() {
+    public Long getId() {
         return id;
     }
     public String getUsername() {
@@ -58,7 +58,7 @@ public class User {
     }
 
     // metodos set
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
     public void setUsername(String username) {
---
+++ src/main/java/com/apirestful/apirestful/services/UserService.java
@@ -0,0 +1,71 @@
+package com.apirestful.apirestful.services;
+
+import com.apirestful.apirestful.models.User;
+import com.apirestful.apirestful.repositories.TaskRepository;
+import com.apirestful.apirestful.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service // definindo como camada de serviço
+public class UserService {
+
+    // definindo comunicação com oo repositories
+    @Autowired //notação para instanciar esses atributos
+    private UserRepository ur;
+    @Autowired //notação para instanciar esses atributos
+    private TaskRepository tr;
+
+    // metodo para encontrar o usuário pelo id
+    public User findById(Long id){
+
+        Optional<User> user = ur.findById(id);
+
+        return user.orElseThrow(() -> new RuntimeException("Usuário não encontrado")); // se o optional conter um eser retorna o user, se nao lança uma exceção
+
+    }
+
+    // metodo para criar o novo usuário
+    @Transactional // usar para realizar insert e update no banco de dados, garante atomicidade
+    public User create(User user){
+
+        user.setId(null); // garante que o id seja gerado no medel, caso o usuári consiga de alguma forma criar o user com id
+
+        user = ur.save(user);
+        tr.saveAll(user.getTasks());
+
+        return user;
+
+    }
+
+//    método para alterar os dados de um usuario
+    @Transactional
+    public User update(User user){
+
+        User newUser = findById(user.getId()); // verifica se o usuário existe
+        newUser.setPassword(user.getPassword());
+
+        return ur.save(newUser);
+
+    }
+
+    // método para deletar o usuário
+    public void delete(Long id){
+
+        findById(id);
+
+        try {
+
+            ur.deleteById(id);
+
+        } catch (Exception e){
+
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas");
+
+        }
+        
+    }
+
+}
